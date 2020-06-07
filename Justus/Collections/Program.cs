aa7d80ad7a32865f91d6f11c7832c627174dd0a8
Justus-coded <justusilemobayo@gmail.com>
2020-06-07 21:51:18 +0100

    Practice on Collections

---
+++ Justus/Collections/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Non -generic collection include
+            Array list
+            Stack
+            Queue
+            Hash Table
+            
+            */
+            
+            // Array List
+            public ArrayList myArrList = new ArrayList();
+
+            public void myArrList(){
+
+                myArrList.Add("Justus");
+                myArrList.Add("32");
+                myArrList.Add("2.3")
+            }
+            public ArrayList getList(){
+                return myArrList;
+            }
+
+            /*Stack
+            
+            First In, Last Out
+            The pop method removes and returns the top value
+            The puch method add the value to the Stack
+            */
+            public Stack myStack = new Stack();
+
+            public void addmyStack(){
+
+                    myStack.Push("Justus");
+                    myStack.Push(34);
+
+            }
+            public object getmyStack(){
+                return myStack;
+            }
+
+            public string RemoveVal(){
+                return myStack.pop();
+            }
+
+        }
+    }
+}
